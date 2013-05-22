5607b517f98abb5309ccf9ed034c8dcda323ef6e
lucyluz333@gmail.com <lucyluz333@gmail.com@aea5be3d-5c29-2158-66dd-78c182cd9976>
2013-05-22 22:16:36 +0000

    Nos dimos cuenta que cuando copiamos en el trapCOW todas las paginas al proceso que quiere escribir, tambien le estamos copiando la pagina de codigo, por eso el hijo se comporta como el padre. Hay que hacer todo bien desde 0

---
+++ xv6/user/forksimple.c
@@ -0,0 +1,32 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+   int pid;
+   pid = fork();
+   switch(pid){
+       
+        case -1:
+                printf(1,"error en el fork\n");
+                break;
+        case 0:
+                printf(1,"soy el hijo mi pid es: %d\n",pid);
+                exit();
+                break;
+         case 4:
+                printf(1,"entreeeeeeeeeeee \n");
+                wait();
+                printf(1,"entreeeeeeeeeeee 2323232\n");
+                printf(1,"soy el padre mi pid es: %d\n",pid);
+                exit();
+                break;
+       default:
+                printf(1,"entre por el default y mi pid es: %d\n",pid);
+    }
+    
+   return 0;
+}
+
