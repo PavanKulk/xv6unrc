a0448b35a6fc76b07b01e2ed19fe035b7633909c
renzobi@gmail.com <renzobi@gmail.com@aea5be3d-5c29-2158-66dd-78c182cd9976>
2013-08-14 20:52:00 +0000

    correcciones por laura... sigue sin andar igual....

--- trunk/xv6/user/forksimple.c
+++ trunk/xv6/user/forksimple.c
@@ -7,23 +7,26 @@ main(void)
 {
    int pid;
    pid = fork();
+   
    switch(pid){
        
-        case -1:
-                printf(1,"forksimple.c---> error en el fork\n");
-                break;
-        case 0:
+        case -1:{
+                 printf(1,"forksimple.c---> error en el fork\n");
+                 break;
+                }
+        case 0:{
                 printf(1,"forksimple.c---> Soy el hijo mi pid es: %d\n",pid);
                 exit();
                 break;
-                
-       default:
+               } 
+       default:{
                 printf(1,"forksimple.c---> Entre al Padre Antes del wait()\n");
                 wait();
                 printf(1,"forksimple.c---> Entre al Padre despues del wait()\n");
-                printf(1,"forksimple.c---> soy el padre mi pid es: %d\n",pid);
+                printf(1,"forksimple.c---> soy el padre el pid de mi hijo es: %d\n",pid);
                 exit();
                 break;
+               }
     }
     
    return 0;
